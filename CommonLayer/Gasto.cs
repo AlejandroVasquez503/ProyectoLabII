55ba8b27de02f9e9d59e4cf2db7e98912bcfbef3
gusrivas <gustavo.rivas1270@gmail.com>
2024-10-08 14:17:05 -0600

    añadiendo commonlayer

---
+++ CommonLayer/Gasto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace commonLayer
+{
+    public class Gasto
+    {
+        public int GastoID { get; set; }
+        public int UsuarioID { get; set; }
+        public int CategoriaID { get; set; }
+        public decimal Monto { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
