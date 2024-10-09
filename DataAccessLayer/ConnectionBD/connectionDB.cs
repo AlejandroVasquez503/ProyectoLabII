8bcd36348ebc9989616df14ab0297e57b3f3643c
gusrivas <gustavo.rivas1270@gmail.com>
2024-10-08 21:29:25 -0600

    conexion de la base hecha

---
+++ DataAccessLayer/ConnectionBD/connectionDB.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class connectionBD
+{
+    private readonly string _connectionString;
+
+    public connectionBD()
+    {
+        _connectionString = "Data Source=DESKTOP-NFDMETJ\\SQLEXPRESS;Initial Catalog=ControlGastosUniversitarios;Integrated Security=True;Encrypt=False;Trust Server Certificate=True";
+    }
+
+    public SqlConnection GetconnectionBD()
+    {
+        return new SqlConnection(_connectionString);
+    }
+}
